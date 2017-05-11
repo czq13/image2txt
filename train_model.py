ae03d1c63b79a3ab356f9383484190d5e15651d0
czq13 <czq92@qq.com>
2017-05-11 19:01:49 +0800

    v0.2

--- train_model.py
+++ train_model.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import caption_generator
+from os import system
 from keras.callbacks import ModelCheckpoint
 
 def train_model(weight = None, batch_size=32, epochs = 10):
@@ -12,7 +13,7 @@ def train_model(weight = None, batch_size=32, epochs = 10):
 
     counter = 0
     file_name = 'weights-improvement-{epoch:02d}.hdf5'
-    checkpoint = ModelCheckpoint(file_name, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    checkpoint = ModelCheckpoint(file_name, monitor='loss', verbose=1, save_best_only=False, mode='min')
     callbacks_list = [checkpoint]
     model.fit_generator(cg.data_generator(batch_size=batch_size), steps_per_epoch=cg.total_samples/batch_size, epochs=epochs, verbose=1, callbacks=callbacks_list)
     try:
@@ -23,4 +24,5 @@ def train_model(weight = None, batch_size=32, epochs = 10):
     print "Training complete...\n"
 
 if __name__ == '__main__':
-    train_model(epochs=50)
+    train_model(weight='weights-improvement-22.hdf5',epochs=50)
+    system('halt')
